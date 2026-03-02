bec93e11564daca3944e2a90855e734c712f92dc
Suresh-Murgan <sureshmurgan195@gmail.com>
2026-03-02 20:05:33 +0530

    Version 3 , Mul and Div Function

--- calculator.py
+++ calculator.py
@@ -4,10 +4,20 @@ def add(a, b):
 def sub(a,b):
     return a - b 
 
+def mul(a,b):
+    return a*b;
+
+def div(a,b):
+    return a/b;
+
 print("--------------------")
+
 print("1.Addition")
 print("2.Subtraction")
-print("3.Exit")
+print("3.Multiplication")
+print("4.Division")
+print("5.Exit")
+
 print("--------------------")
 
 status = True
@@ -30,7 +40,19 @@ while status:
             print("--------------------")
         
         case 3:
-            exit()
+            num1 = int(input("Enter the Num 1 : "))
+            num2 = int(input("Enter the Num 2 : "))
+            print(mul(num1, num2))
+            print("--------------------")
         
+        case 4:
+            num1 = int(input("Enter the Num 1 : "))
+            num2 = int(input("Enter the Num 2 : "))
+            print(div(num1, num2))
+            print("--------------------")
+            
+        case 5:
+            status=False
+           
         case _:
             print("Enter a valid Option...")
