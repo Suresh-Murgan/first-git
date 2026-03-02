dd89395c93d808e019bb0dd73ac7ab7c73c7d90a
Suresh-Murgan <sureshmurgan195@gmail.com>
2026-03-02 19:56:31 +0530

    Version 2 , Subtraction Function

--- calculator.py
+++ calculator.py
@@ -1,8 +1,14 @@
 def add(a, b):
     return a + b
 
+def sub(a,b):
+    return a - b 
+
+print("--------------------")
 print("1.Addition")
-print("2.Exit")
+print("2.Subtraction")
+print("3.Exit")
+print("--------------------")
 
 status = True
 
@@ -10,12 +16,21 @@ while status:
     choice = int(input("Enter the Choice : "))
 
     match choice:
+
         case 1:
             num1 = int(input("Enter the Num 1 : "))
             num2 = int(input("Enter the Num 2 : "))
             print(add(num1, num2))
             print("--------------------")
+
         case 2:
-            status = False
+            num1 = int(input("Enter the Num 1 : "))
+            num2 = int(input("Enter the Num 2 : "))
+            print(sub(num1, num2))
+            print("--------------------")
+        
+        case 3:
+            exit()
+        
         case _:
             print("Enter a valid Option...")
